8c371f890700b77401c1673e10456b0edfacd3c3
manpreetplassi <manugirn07gmail.com>
2026-01-06 17:11:52 +0530

    change the backend stack into nestjs

--- routes/appointments.js
+++
@@ -1,7 +0,0 @@
-const { createAppointment, getAppointment } = require("../controller/appointments")
-const router = require("express").Router()
-
-router.post("/create", createAppointment)
-router.patch("/get", getAppointment)
-
-module.exports = router
--- routes/user.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router()
-const { signUp, login } = require("../controller/user")
-
-router.post("/register", signUp)
-router.post("/login", login)
-
-module.exports = router
